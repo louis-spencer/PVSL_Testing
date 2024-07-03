b7fcbc8586b44f8a6902e885d8a95bb5231eadfc
louis-spencer <louis.elliot.spencer@gmail.com>
2024-07-03 16:54:26 -0400

    modified main.py, app.ui

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from MainWindow import Ui_MainWindow
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
 
+from MainWindow import Ui_MainWindow
+from duration_dialog import Ui_Duration
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-
-        self.update_datetime()
+        self.setWindowTitle("PV Testing")
         self.save.clicked.connect(self.save_file_dialog)
         self.controls_settings_tab.tabBarClicked.connect(self.update_datetime)
+        self.duration.currentIndexChanged.connect(self.open_duration_dialog)
 
     def save_file_dialog(self):
         """
@@ -45,12 +48,33 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         current_QTime = QtCore.QTime.currentTime()
         self.date_time.setDate(current_QDate)
         self.date_time.setTime(current_QTime)
+        
+    def open_duration_dialog(self, arg):
+        if arg == 0: self.duration = -1
+        elif arg == 1: self.duration = 10
+        elif arg == 2: self.duration = 20
+        elif arg == 3: self.duration = 30
+        elif arg == 4: self.duration = 45
+        elif arg == 5: self.duration = 60
+        elif arg == 6: self.duration = 75
+        elif arg == 9:
+            dialog = DurationDialog(self)
+            dialog.exec()
+    
+class DurationDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        print(parent)
+        super().__init__(parent)
+        self.ui = Ui_Duration()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Set Duration")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     # as opposed to window.show(), opens on fullscreen
-    window.showMaximized()
+    #window.showMaximized()
+    window.show()
     sys.exit(app.exec_())
 """
 NOTE: pyqtgraph might be faster than matplotlib, worth checking out?
